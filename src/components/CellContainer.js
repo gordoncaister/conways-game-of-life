89cf9665123cecb043e04214195e89f01db17fe7
Gordon Caister <gordoncaister@gmail.com>
2020-06-21 20:59:38 -0500

    added the ability to click on a cell working

--- src/components/CellContainer.js
+++ src/components/CellContainer.js
@@ -1,41 +1,47 @@
 import React,{useState, useEffect} from 'react';
 import {Cell} from "./Cell"
 
+let mutation=0;
+
 export function CellContainer() {
     let cols = 10;
     let rows = 10;
 
-    let startGrid = (cols,rows)=> {
+    const startGrid = (cols,rows)=> {
+
         let arr = new Array(cols);
         for(let i = 0; i < arr.length; i++){
             arr[i] = new Array(rows);
+            for(let j = 0; j < rows; j++){
+                arr[i][j] = 0
+            }
         }
         return arr;
     }
 
-    let [grid,setGrid] = useState(startGrid(rows,cols))
-
-    for(let i = 0; i< cols; i++){
-        for(let j = 0; j < rows; j++){
-            grid[i][j] = 0
-        }
-    }
+    let [grid,setGrid] = useState(startGrid(cols, rows))
+    let [next,setNext] = useState(startGrid(cols, rows))
+    
 
     const onCellClick = (col,row)=>{
-        let tempGrid = grid
+        let tempGrid = [...grid]
+        
         if(tempGrid[col][row]){
+            console.log("was one,now zero")
             tempGrid[col][row] = 0
         } else {
+            console.log("was zero,now one")
             tempGrid[col][row] = 1
+            console.log(tempGrid[col][row])
         }
-        return setGrid(tempGrid)
-        console.log(`${col},${row}: ${grid[col][row]}`)
+        console.log("TEMPGRID:", tempGrid)
+        setNext(tempGrid)
+        
     }
-    
 
-
-    
-  
+    useEffect(()=>{
+        setGrid(next)
+    },[next])
 
     return (
         <>
@@ -43,7 +49,7 @@ export function CellContainer() {
             <div className="cell-container" style={{width:`${cols*18}px`}}>
                 {
                     grid.map((rows,i) => {
-                        console.log("render")
+                        console.log("render",grid[0][0])
                         return (
                             rows.map((cols,j) => {
                                 
